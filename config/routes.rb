630cdf66ac1323e9cce35038c52a1893c5c5bb81
Ben Weible <benweible@yahoo.com>
2015-06-24 05:06:31 +0000

    form partials and other stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
+
 Rails.application.routes.draw do
   
 root 'pages#home'
 get '/home', to: 'pages#home'
-  
+
+
+#get '/recipes', to: 'recipes#index'
+#get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+#post '/recipes', to: 'recipes#create'
+#get '/recipes/edit', to: 'recipes#edit', as: 'edit_recipe'
+#patch 'recipes/:id', to: 'recipe#update'
+#get 'recipes/:id', to: 'recipe#show'
+#delete 'recipes/:id', to: 'recipe#destroy'
+
+resources :recipes
+
+
  
 end
